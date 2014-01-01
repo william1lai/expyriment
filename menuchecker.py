0a528358ace84e3ddd641f9dd8ae43a3120f5795
William Lai <williamlai@ucla.edu>
2014-01-01 09:58:35 -0800

    menuchecker 2.0
    Now has option to look at lunch menu, and also scores the menus and
    recommends where to eat.

--- menuchecker.py
+++ menuchecker.py
@@ -5,16 +5,6 @@ import string
 from HTMLParser import HTMLParser
 from htmlentitydefs import name2codepoint
 
-menu_url = "http://m.dining.ucla.edu/menu/index.cfm?r=De%20Neve,Hedrick%20Dining,Covel%20Dining,Feast%20at%20Rieber&restaurantType=Residential"
-
-hi = "&m=Dinner&d=01%2F05%2F2014"
-
-aparser = argparse.ArgumentParser(description='''Find out how good the menu is today. BruinPlate not supported because it's not supported on the UCLA mobile dining page''')
-aparser.add_argument('-l', '--lunch', action='store_true')
-aparser.add_argument('infile', nargs='?')
-
-tagofinterest = "div class=menu-full menu-detailed"
-
 class MyHTMLParser(HTMLParser):
     def reset(self):
         HTMLParser.reset(self)
@@ -68,106 +58,163 @@ class MyHTMLParser(HTMLParser):
     def handle_decl(self, data):
         pass
 
+def main():
+    menu_url = "http://m.dining.ucla.edu/menu/index.cfm?r=De%20Neve,Hedrick%20Dining,Covel%20Dining,Feast%20at%20Rieber&restaurantType=Residential"
+    aparser = argparse.ArgumentParser(description='''Find out how good the menu is today. BruinPlate not supported because it's not supported on the UCLA mobile dining page''')
+    aparser.add_argument('-l', '--lunch', action='store_true')
+    aparser.add_argument('infile', nargs='?')
 
-parser = MyHTMLParser()
+    parser = MyHTMLParser()
+    canopener = urllib.FancyURLopener({})
+    args = aparser.parse_args()
 
-canopener = urllib.FancyURLopener({})
-args = aparser.parse_args()
+    if args.lunch:
+        menu_url = menu_url + "&m=Lunch"
+    else:
+        menu_url = menu_url + "&m=Dinner"
 
-if args.lunch:
-    menu_url = menu_url + "&m=Lunch"
-else:
-    menu_url = menu_url + "&m=Dinner"
+    print menu_url
 
-print menu_url
+    if not args.infile:
+        f = canopener.open(menu_url)
+    else:
+        f = open(args.infile, 'r')
+    s = f.read()
+    parser.feed(s)
 
-if not args.infile:
-    f = canopener.open(menu_url)
-else:
-    f = open(args.infile, 'r')
-s = f.read()
-parser.feed(s)
+    covel = '\n'.join(parser.coveldata)
+    deneve = '\n'.join(parser.denevedata)
+    feast = '\n'.join(parser.feastdata)
+    print "Covel Menu:\n", covel
+    print "\nDe Neve Menu:\n", deneve
+    print "\nFeast Menu:\n", feast
 
-covel = '\n'.join(parser.coveldata)
-deneve = '\n'.join(parser.denevedata)
-feast = '\n'.join(parser.feastdata)
-print "Covel Menu:\n", covel
-print "\nDe Neve Menu:\n", deneve
-print "\nFeast Menu:\n", feast
+    edislikes = [ "mushroom", "shrimp", "seafood", "sushi" ]
+    elikes = [ "pho", "spaghetti", "tacos" ]
+    bdislikes = [ ]
+    blikes = [ "fish", "mahi", "baked potato", "shrimp" ]
+    wlikes = [ "potstickers", "sushi" ]
+    edec = False
+    bdec = False
+    wdec = False
+    covelscore = 0
+    denevescore = 0
+    feastscore = 0
 
-ericdislikes = [ "mushroom", "shrimp", "seafood" ]
-ericlikes = [ "pho", "spaghetti", "tacos" ]
-briandislikes = [ "pepper shrimp" ]
-brianlikes = [ "fish", "mahi", "baked potato", "shrimp" ]
-edec = False
-bdec = False
+    print ""
+    for item in parser.coveldata:
+        for food in edislikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Eric won't like Covel because of", item
+                edec = True
+                covelscore = covelscore - 15
+    for item in parser.denevedata:
+        for food in edislikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Eric won't like De Neve because of", food
+                edec = True
+                denevescore = denevescore - 15
+    for item in parser.feastdata:
+        for food in edislikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Eric won't like Feast because of", food
+                edec = True
+                feastscore = feastscore - 15
+    for item in covel:
+        for food in elikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Eric will like Covel because of", food
+                edec = True
+                covelscore = covelscore + 10
+    for item in parser.denevedata:
+        for food in elikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Eric will like De Neve because of", food
+                edec = True
+                denevescore = denevescore + 10
+    for item in parser.feastdata:
+        for food in elikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Eric will like Feast because of", food
+                edec = True
+                feastscore = feastscore + 10
 
-print ""
-for item in parser.coveldata:
-    for food in ericdislikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Eric won't like Covel because of", item
-            edec = True
-for item in parser.denevedata:
-    for food in ericdislikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Eric won't like De Neve because of", food
-            edec = True
-for item in parser.feastdata:
-    for food in ericdislikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Eric won't like Feast because of", food
-            edec = True
-for item in covel:
-    for food in ericlikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Eric will like Covel because of", food
-            edec = True
-for item in parser.denevedata:
-    for food in ericlikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Eric will like De Neve because of", food
-            edec = True
-for item in parser.feastdata:
-    for food in ericlikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Eric will like Feast because of", food
-            edec = True
+    for item in parser.coveldata:
+        for food in bdislikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Brian won't like Covel because of", food
+                bdec = True
+                covelscore = covelscore - 15
+    for item in parser.denevedata:
+        for food in bdislikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Brian won't like De Neve because of", food
+                bdec = True
+                denevescore = denevescore - 15
+    for item in parser.feastdata:
+        for food in bdislikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Brian won't like Feast because of", food
+                bdec = True
+                feastscore = feastscore - 15
+    for item in parser.coveldata:
+        for food in blikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Brian will like Covel because of", food
+                bdec = True
+                covelscore = covelscore + 10
+    for item in parser.denevedata:
+        for food in blikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Brian will like De Neve because of", food
+                bdec = True
+                denevescore = denevescore + 10
+    for item in parser.feastdata:
+        for food in blikes:
+            if string.find(string.lower(item), food) != -1:
+                print "Brian will like Feast because of", food
+                bdec = True
+                feastscore = feastscore + 10
 
-for item in parser.coveldata:
-    for food in briandislikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Brian won't like Covel because of", food
-            bdec = True
-for item in parser.denevedata:
-    for food in briandislikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Brian won't like De Neve because of", food
-            bdec = True
-for item in parser.feastdata:
-    for food in briandislikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Brian won't like Feast because of", food
-            bdec = True
-for item in parser.coveldata:
-    for food in brianlikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Brian will like Covel because of", food
-            bdec = True
-for item in parser.denevedata:
-    for food in brianlikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Brian will like De Neve because of", food
-            bdec = True
-for item in parser.feastdata:
-    for food in brianlikes:
-        if string.find(string.lower(item), food) != -1:
-            print "Brian will like Feast because of", food
-            bdec = True
+    for item in parser.coveldata:
+        for food in wlikes:
+            if string.find(string.lower(item), food) != -1:
+                print "William will like Covel because of", food
+                wdec = True
+                covelscore = covelscore + 10
+    for item in parser.denevedata:
+        for food in wlikes:
+            if string.find(string.lower(item), food) != -1:
+                print "William will like De Neve because of", food
+                wdec = True
+                denevescore = denevescore + 10
+    for item in parser.feastdata:
+        for food in wlikes:
+            if string.find(string.lower(item), food) != -1:
+                print "William will like Feast because of", food
+                wdec = True
+                feastscore = feastscore + 10
 
-if not edec:
-    print "Eric doesn't care"
-if not bdec:
-    print "Brian doesn't care"
+    if not edec:
+        print "Eric doesn't care"
+    if not bdec:
+        print "Brian doesn't care"
+    if not wdec:
+        print "William doesn't care"
 
-print "Victor doesn't care\n"
+    print "Victor doesn't care\n"
+
+    m = max(covelscore, denevescore, feastscore)
+    if m < -20:
+        print "This program recommends quick service.\n"
+    elif m == feastscore:
+        print "This program recommends Feast.\n"
+    elif m == denevescore:
+        print "This program recommends De Neve.\n"
+    else:
+        print "This program recommends Covel.\n"
+    raw_input() #to keep window open
+
+
+if __name__ == "__main__":
+    main()
